445211f00e8042cb0b4a3bdb384d575914f6f29a
Filip Vičar <Paravan3010@gmail.com>
2024-04-03 12:17:16 +0200

    Optional properties in domain model. (#28)

--- Domain/Article.cs
+++ Domain/Article.cs
@@ -7,17 +7,17 @@
         public string FilePath { get; set; } = String.Empty;
         public string PreviewPhotoPath { get; set; } = String.Empty;
         public string ShortTitle { get; set; } = String.Empty;
-        public string LongTitle { get; set; }
+        public string LongTitle { get; set; } = String.Empty;
         public string TextBeginning { get; set; } = String.Empty;
         public string PageTitle { get; set; } = String.Empty;
         public string PageDescription { get; set; } = String.Empty;
-        public Category Category { get; set; }
+        public Category? Category { get; set; }
         public DateTime Created { get; set; } = DateTime.Today;
         public DateTime LastUpdated { get; set; } = DateTime.Today;
         public bool IsPublished { get; set; } = false;
         public string GalleryUrl { get; set; } = String.Empty;
         public int NumberOfVisits { get; set; }
-        public Article PreviousArticle { get; set; }
-        public Article NextArticle { get; set; }
+        public Article? PreviousArticle { get; set; }
+        public Article? NextArticle { get; set; }
     }
 }
