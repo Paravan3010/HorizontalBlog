cd73f5e58632c489122afdcf80859fa98e94dd3a
Filip Vičar <Paravan3010@gmail.com>
2025-02-27 22:32:54 +0300

    temp save

--- Horizontal/Controllers/ContactController.cs
+++ Horizontal/Controllers/ContactController.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Horizontal.Models;
+using Horizontal.Domain.Repositories;
+using Horizontal.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Horizontal.Controllers
 {
     public class ContactController : Controller
     {
+        private readonly INavigationService _navigationService;
+        private readonly ITagRepository _tagRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public ContactController(INavigationService navigationService, ITagRepository tagRepository, ICategoryRepository categoryRepository)
+        {
+            _navigationService = navigationService;
+            _tagRepository = tagRepository;
+            _categoryRepository = categoryRepository;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var model = new ContactModel(_navigationService, _tagRepository, _categoryRepository);
+            return View(model);
         }
     }
 }
---
+++ Horizontal/Models/ContactModel.cs
@@ -0,0 +1,15 @@
+﻿using Horizontal.Domain.Repositories;
+using Horizontal.Models;
+using Horizontal.Services;
+
+namespace Horizontal.Models
+{
+    public class ContactModel : BaseModel
+    {
+        public ContactModel(INavigationService navigationService, 
+                            ITagRepository tagRepository, 
+                            ICategoryRepository categoryRepository) : base(navigationService, tagRepository, categoryRepository)
+        {
+        }
+    }
+}
